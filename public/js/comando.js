3a730e42766009a0c17485a6300f056f3fd1a81b
LAPTOP-0O7C38OE\JOSE DAVID <lucariomon2@gmail.com>
2020-03-09 09:26:38 -0500

    Parcial1

---
+++ public/js/comando.js
@@ -0,0 +1,10 @@
+ 
+function formFin(){
+    document.getElementById('formularioIni').style.display = 'none';
+    var boton = document.getElementById('ofertaBtn');
+    boton.disabled = false;
+}
+
+function ofertar(){
+    
+}
