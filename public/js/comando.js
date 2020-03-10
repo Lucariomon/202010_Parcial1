cc8c34ef602c5fd00dd65fcd8ccdf185660f801d
LAPTOP-0O7C38OE\JOSE DAVID <lucariomon2@gmail.com>
2020-03-09 21:56:28 -0500

    Entrega Final

--- public/js/comando.js
+++ public/js/comando.js
@@ -5,6 +5,43 @@ function formFin(){
     boton.disabled = false;
 }
 
-function ofertar(){
-    
+$("#regisBtn").on("click",(function(e){
+    e.preventDefault();
+}));
+
+var ofertaActual = 150000000;
+var aleatorio = (Math.random()* 5000000 + 5000000);
+
+function aumentar(){
+    ofertaActual = Math.abs(Math.abs(ofertaActual) + Math.abs(aleatorio.toFixed(2)));
 }
+
+    
+    document.getElementById("valor").innerHTML = ofertaActual;
+
+    function actualizarValor(){
+        aumentar();
+        document.getElementById("valor").innerHTML = ofertaActual;
+    }
+
+
+document.getElementById('ofertaBtn').addEventListener('click',function(){
+    var boton = document.getElementById('ofertaBtn');
+    boton.disabled = true;
+    var pb = (Math.random()* 0.5 + 0.3);
+    var po = (Math.random()* 0.5 + 0.3);
+    if(po > pb){
+        console.log("succedd");
+        actualizarValor();
+        setTimeout(ver,30000);
+        alert("aceptada");
+    }
+    console.log("failed")
+    alert("no aceptada")
+    setTimeout(ver,1000);
+})
+
+function ver(){
+    var boton = document.getElementById('ofertaBtn');
+    boton.disabled = false;
+};
